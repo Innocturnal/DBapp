bf32429f0fef81244a0c89e0a47a42fa7a98bec3
Dima <dimoniwe2@mail.ru>
2023-10-12 01:18:51 +0300

    cycle rework

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "20");
+let numberOfFilms;
+do {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "20");
+} while (
+  numberOfFilms == "" ||
+  numberOfFilms == null ||
+  numberOfFilms.length > 50
+);
+
 let lastFilm;
 let lastMark;
 
@@ -12,13 +20,24 @@ const personaMovieDB = {
   privat: false,
 };
 
+if (personaMovieDB.count > 0 && personaMovieDB.count <= 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personaMovieDB.count > 10 && personaMovieDB.count <= 30) {
+  alert("Вы рядовой зритель");
+} else if (personaMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
+
 function askFilm() {
   lastFilm = prompt("Какой последний фильм вы посмотрели?", "Ohota");
   lastMark = +prompt("На сколько оцените его?", "8.1");
   personaMovieDB.movies[lastFilm] = lastMark;
 }
 
-askFilm();
-askFilm();
+for (let i = 0; i <= 1; i++) {
+  askFilm();
+}
 
 console.log(personaMovieDB);
