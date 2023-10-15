fdda0b82efe38a1ff242de029647b54be7d346d3
Dima <dimoniwe2@mail.ru>
2023-10-15 18:03:58 +0300

    reorganized by functions

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ let numberOfFilms;
 let lastFilm;
 let lastMark;
 
-do {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "20");
-} while (
-  numberOfFilms == "" ||
-  numberOfFilms == null ||
-  numberOfFilms.length > 50
-);
+function start() {
+  do {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "20");
+  } while (
+    numberOfFilms == "" ||
+    numberOfFilms == null ||
+    isNaN(numberOfFilms)
+  );
+}
+
+start();
 
 const personaMovieDB = {
   count: numberOfFilms,
@@ -30,8 +34,17 @@ if (personaMovieDB.count > 0 && personaMovieDB.count <= 10) {
   alert("Произошла ошибка");
 }
 
+function showMyDB(status) {
+  if (status === false) {
+    console.log(personaMovieDB);
+  }
+}
+
+showMyDB(personaMovieDB.privat);
+
 function askFilm() {
-  for (let i = 0; i < 3; i++) {
+  let i = 0;
+  do {
     lastFilm = prompt("Какой последний фильм вы посмотрели?", "Ohota");
     lastMark = +prompt("На сколько оцените его?", "8.1");
     if (
@@ -41,13 +54,22 @@ function askFilm() {
       lastMark != null
     ) {
       personaMovieDB.movies[lastFilm] = lastMark;
+      i++;
     } else {
       console.log("error");
       i--;
     }
+  } while (i < personaMovieDB.count);
+}
+
+function askGenres() {
+  for (let i = 0; i < 2; i++) {
+    personaMovieDB.genres[i] = prompt(
+      `Ваш любимый жанр под номером ${i+1}`,
+      "Jopa"
+    );
   }
 }
 
 askFilm();
-
-console.log(personaMovieDB);
+askGenres();
